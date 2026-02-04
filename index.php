d11025bc2ea639c0ddf1bbf6f17ece2a2031da8d
Pietro Siboni <pietro.siboni3@studio.unibo.it>
2026-02-04 18:28:53 +0100

    Update index with receiving success messages

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ require_once 'bootstrap.php';
 
 /* 1. GESTIONE LOGIN / SESSIONE REALE */
 // Recuperiamo l'ID solo se esiste, altrimenti mettiamo null
+if (isset($_GET['msg'])) {
+    $templateParams["messaggio_successo"] = $_GET['msg'];
+}
 $idUtenteLoggato = $_SESSION["id_utente"] ?? null;
 
 if ($idUtenteLoggato) {
